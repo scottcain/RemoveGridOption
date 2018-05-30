0eb604a5d416e641694b112ba361973a73db4912
Scott Cain <scott+git@scottcain.net>
2018-05-30 14:06:13 +0000

    clean up and removing debugging output

--- js/main.js
+++ js/main.js
@@ -37,30 +37,15 @@ return declare( JBrowsePlugin,
     },
 
     toggleGridLines: function() {
-        //get the current value of gridlines_major and minor
-        //var gridline_minor = style || lang.hitch( this, 'getStyle' );
-        //var gridline_major = this.style.gridline_major.border;
-
         //get all of the gridline divs
 
-        console.log(query('.gridline').style('border-style').at(0,1));
-
-        if (array.some(query('.gridline').style('border-style'), function(item) { return item == 'none none none solid' ; })) {
+        if (array.some(query('.gridline').style('border-style'), 
+                  function(item) { return item == 'none none none solid' ; })) {
             query('.gridline').style('border-style', 'none none none none'); 
         }
         else {
             query('.gridline').style('border-style', 'none none none solid');
         }
- 
-        //query('.gridline').style('border-style', 'none none none none');
-        //console.log(nl);
-        //domClass.add(win.body(), 'gridline_major');
-        //console.log(win.body());
-        //console.log(lang.hitch(this.browser.config, 'getStyle'));
-        //console.log(gridline_minor);        
-
-        //invert them
     }
-
 });
 });
